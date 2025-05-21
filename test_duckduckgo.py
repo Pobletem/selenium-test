dcffb8ce5a68bbe6d3e3857ea175fda7eb60dc86
Martin Poblete <martinpoblete9166@outlook.com>
2025-05-21 06:23:22 -0500

    Clean up python script code using flake8 linter

--- test_duckduckgo.py
+++ test_duckduckgo.py
@@ -2,28 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import logging
+# import logging
+
 
 def test_duckduckgo_search():
     driver = webdriver.Chrome()
     driver.get("https://duckduckgo.com")
 
     try:
-        # Set up logging
-        # Sets logging level to info and makes the format display the level name and then a message
-        # logging.basicConfig(level=logging.INFO, format='\n%(levelname)s: %(message)s\n')
-
-        # Prints the title of the website for logging reasons
-        # logging.info("Title is: %s", driver.title)
-
         # QA Concept: Assert correct page title
         assert "DuckDuckGo" in driver.title, "FAIL: Title mismatch"
 
-        print(f"PASS: Title matches what was searched")
+        print("PASS: Title matches what was searched")
 
         # Locate the search box element and search a term
         # q is the element name of the search box, it stands for 'query'
-        searchBox = driver.find_element(By.NAME, "q") 
+        searchBox = driver.find_element(By.NAME, "q")
         searchBox.send_keys("Selenium WebDriver" + Keys.RETURN)
 
         time.sleep(3)
@@ -36,9 +30,10 @@ def test_duckduckgo_search():
         assert len(results) > 0, "FAIL: No results found"
 
         # Printing all the results found
-        print(f"PASS: Found {len(results)} results")
+        print("PASS: Found {len(results)} results")
     finally:
         driver.quit()
+
 
 def test_valid_login():
     driver = webdriver.Chrome()
@@ -49,7 +44,7 @@ def test_valid_login():
         # finds the username and password fields
         passwordField = driver.find_element(By.ID, "password")
         usernameField = driver.find_element(By.ID, "username")
-        # Inputs valid credentials in this case username: tomsmith, password: SuperSecretPassword!
+        # Inputs valid credentials
         usernameField.send_keys("tomsmith")
         passwordField.send_keys("SuperSecretPassword!")
 
@@ -57,25 +52,29 @@ def test_valid_login():
         driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
         # QA Assertion: Check that login was successful
-        # finds the class of the flash ui element which is used to display a success or error message after a login
+        # finds the class of the flash ui element
         flashClass = driver.find_element(By.ID, "flash")
-        # Saving the text that shows up in the alert message when a login attempt is made
+        # Saving the text that shows up in the alert message
         successMessage = flashClass.text
-    
-        # Testing to see if the class of the flash msg message is the same class as we expect of the success message 
-        assert "success" in flashClass.get_attribute("class"), "FAIL: Unexpected class"
 
-        print(f"PASS: Valid login flash class used")
+        # Test class of the flash ui ensuring its correct
+        assert "success" in flashClass.get_attribute("class"), (
+            "FAIL: Unexpected class"
+        )
 
-        # if the text inside the flash msg is the same as the success msg then we know the login succeeded if its not then the login failed
-        assert "You logged into a secure area!" in successMessage, "FAIL: Login failed"
+        print("PASS: Valid login flash class used")
 
-        print(f"PASS: Valid login successful")
+        # Test text inside flash ui ensuring its correct
+        assert "You logged into a secure area!" in successMessage, (
+            "FAIL: Login failed"
+        )
 
+        print("PASS: Valid login successful")
 
         time.sleep(3)
     finally:
         driver.quit()
+
 
 def test_invalid_login():
     driver = webdriver.Chrome()
@@ -86,7 +85,7 @@ def test_invalid_login():
         # finds the username and password fields
         passwordField = driver.find_element(By.ID, "password")
         usernameField = driver.find_element(By.ID, "username")
-        # Inputs invalid credentials 
+        # Inputs invalid credentials
         usernameField.send_keys("wronguser")
         passwordField.send_keys("superwrongpassword")
 
@@ -94,29 +93,30 @@ def test_invalid_login():
         driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
         # QA Assertion: Check that login was a failure
-        # finds the class of the flash ui element which is used to display a success or error message after a login
+        # finds the class of the flash ui element
         flashClass = driver.find_element(By.ID, "flash")
-        # Saving the text that shows up in the alert message when a login attempt is made
+        # Saving the text that shows up in the alert message
         errorMessage = flashClass.text
-    
-        # Testing to see if the class of the flash msg message is the same class as we expect of the error message 
-        assert "error" in flashClass.get_attribute("class"), "FAIL: Unexpected class"
 
-        print(f"PASS: Proper failure login flash class used")
+        # Test class of the flash ui ensuring it matches the error class
+        assert "error" in flashClass.get_attribute("class"), (
+            "FAIL: Unexpected class"
+        )
+        print("PASS: Proper failure login flash class used")
 
-        # if the text inside the flash msg is the same as the error msg then we know the login failed properly
-        assert "Your username is invalid!" in errorMessage, "FAIL: Invalid login not handled properly"
+        # Test text of the flash ui ensuring its correct
+        assert "Your username is invalid!" in errorMessage, (
+            "FAIL: Invalid login not handled properly"
+        )
 
-        print(f"PASS: Invalid login properly handled")
-
+        print("PASS: Invalid login properly handled")
 
         time.sleep(3)
     finally:
         driver.quit()
 
+
 if __name__ == "__main__":
     test_duckduckgo_search()
     test_valid_login()
     test_invalid_login()
-
- 
