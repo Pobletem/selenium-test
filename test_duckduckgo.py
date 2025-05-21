64a84067e039202df5f1caab2d8eb5cf9b7e094f
Martin Poblete <martinpoblete9166@outlook.com>
2025-05-21 04:25:41 -0500

    Add main block to py script

--- test_duckduckgo.py
+++ test_duckduckgo.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 
+
 def test_duckduckgo_search():
     driver = webdriver.Chrome()
     driver.get("https://duckduckgo.com")
 
     # QA Concept: Assert correct page title
     assert "DuckDuckGo" in driver.title, "FAIL: Title mismatch"
+
+if __name__ == "__main__":
+    test_duckduckgo_search()
+
+ 
