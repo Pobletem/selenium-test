8d99d6bd8f9ee12e8dc6226f7e586c5be7853ba0
Martin Poblete <martinpoblete9166@outlook.com>
2025-05-21 04:05:54 -0500

    Add section to check for correct web title in script

---
+++ test_duckduckgo.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+def test_duckduckgo_search():
+    driver = webdriver.Chrome()
+    driver.get("https://duckduckgo.com")
+
+    # QA Concept: Assert correct page title
+    assert "DuckDuckGo" in driver.title, "FAIL: Title mismatch"
