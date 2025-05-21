eff27f4602f46c741040b157daff499f5fec6d43
Martin Poblete <martinpoblete9166@outlook.com>
2025-05-21 04:26:53 -0500

    Add user input check to stop the script

--- test_duckduckgo.py
+++ test_duckduckgo.py
@@ -8,6 +8,10 @@ def test_duckduckgo_search():
     # QA Concept: Assert correct page title
     assert "DuckDuckGo" in driver.title, "FAIL: Title mismatch"
 
+    # Pause so the browser stays open
+    input("Press Enter to close the browser...")
+    driver.quit()
+
 if __name__ == "__main__":
     test_duckduckgo_search()
 
