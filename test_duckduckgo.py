86c30a5a02ed46a0bbab0b96286880bd605ee440
Martin Poblete <martinpoblete9166@outlook.com>
2025-05-21 04:29:26 -0500

    Import logging and add logging msgs

--- test_duckduckgo.py
+++ test_duckduckgo.py
@@ -1,9 +1,17 @@
 from selenium import webdriver
-
+import logging
 
 def test_duckduckgo_search():
     driver = webdriver.Chrome()
     driver.get("https://duckduckgo.com")
+
+    # Set up logging
+
+    # Sets logging level to info and makes the format display the level name and then a message
+    logging.basicConfig(level=logging.INFO, format='\n%(levelname)s: %(message)s\n')
+
+    # Prints the title of the website for logging reasons
+    logging.info("Title is: %s", driver.title)
 
     # QA Concept: Assert correct page title
     assert "DuckDuckGo" in driver.title, "FAIL: Title mismatch"
