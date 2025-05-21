fc16b5cb46dcf52feabded3ed57afb662aea984d
Martin Poblete <martinpoblete9166@outlook.com>
2025-05-21 04:44:23 -0500

    Add way to check if results appeared after a search to script

--- test_duckduckgo.py
+++ test_duckduckgo.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import logging
 
 def test_duckduckgo_search():
@@ -6,7 +9,6 @@ def test_duckduckgo_search():
     driver.get("https://duckduckgo.com")
 
     # Set up logging
-
     # Sets logging level to info and makes the format display the level name and then a message
     logging.basicConfig(level=logging.INFO, format='\n%(levelname)s: %(message)s\n')
 
@@ -15,6 +17,20 @@ def test_duckduckgo_search():
 
     # QA Concept: Assert correct page title
     assert "DuckDuckGo" in driver.title, "FAIL: Title mismatch"
+
+    # Locate the search box element and search a term
+    # q is the element name of the search box, it stands for 'query'
+    searchBox = driver.find_element(By.NAME, "q") 
+    searchBox.send_keys("Selenium WebDriver" + Keys.RETURN)
+
+    # Here we are grabbing all the elements in the css class result that are inside the css class results
+    results = driver.find_elements(By.CSS_SELECTOR, ".results .result")
+
+    # QA concept, ensuring there are results showing as expected
+    assert len(results) > 0, "FAIL: No results found"
+
+    # Printing all the results found
+    print(f"PASS: Found {len(results)} results")
 
     # Pause so the browser stays open
     input("Press Enter to close the browser...")
